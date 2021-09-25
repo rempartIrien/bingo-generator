234b684f9c9a7d97c5655e39f824c428731c6c22
rempartIrien <pierre.martin@live.com>
2021-09-25 12:31:09 +0200

    Handle bad numbers in bingo viewer

--- src/bingo/BingoViewer.tsx
+++ src/bingo/BingoViewer.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import { shuffleArray, toGrid } from './random.util';
 
 interface BingoViewerProps {
+  title: string;
   labels: string[];
   rowNumber: number;
   columnNumber: number;
 }
 
 function BingoViewer(props: BingoViewerProps): JSX.Element {
+  if (!isNumberValid(props.rowNumber) || !isNumberValid(props.columnNumber)) {
+    return <p>Can&apos;t generate bingo, please check your parameters.</p>;
+  }
   const randomizedStuff: string[][] = toGrid(
     shuffleArray(props.labels),
     props.rowNumber,
@@ -16,18 +20,25 @@ function BingoViewer(props: BingoViewerProps): JSX.Element {
   );
 
   return (
-    <table>
-      <tbody>
-        {randomizedStuff.map((array, index) => (
-          <tr key={index}>
-            {array.map((value, i) => (
-              <td key={`${index}_${i}`}>{value}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <h2>{props.title}</h2>
+      <table>
+        <tbody>
+          {randomizedStuff.map((array, index) => (
+            <tr key={index}>
+              {array.map((value, i) => (
+                <td key={`${index}_${i}`}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
+}
+
+function isNumberValid(value: number): boolean {
+  return !!(value || value === 0);
 }
 
 export default BingoViewer;
